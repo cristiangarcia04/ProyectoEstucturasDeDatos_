2fc14ef2e2754b5743b897c5747de3c03ba1ffda
cristian <cristiandavidgarcia10@gmail.com>
2026-05-23 18:00:08 -0500

    logica

--- entidades/Sospechoso.cpp
+++ entidades/Sospechoso.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "Sospechoso.h"
+#include <iostream>
+using namespace std;
+
+Sospechoso::Sospechoso() {
+    nombre = "";
+    culpable = false;
+}
+
+Sospechoso::Sospechoso(string nombre, vector<string> atributos) {
+    nombre = nombre;
+    culpable = false;
+    atributos = atributos;
+}
+
+void Sospechoso::revelar(int indice) {
+    if (indice > atributos.size()) {
+        revelados.push_back((atributos)[indice]);
+        indice++;
+    }
+}
+
+void Sospechoso::mostrar() {
+    cout << " " << nombre << " | atributos: ";
+    if (revelados.empty()) {
+        cout << "-";
+    } else {
+        for (int i = 0; i < revelados.size(); i++) {
+            cout << revelados[i];
+            if (i != revelados.size() - 1) cout << ", ";
+        }
+    }
+    cout << endl;
+}
+
+
+
--- entidades/Sospechoso.h
+++ entidades/Sospechoso.h
@@ -4,13 +4,20 @@
 
 #ifndef SOSPECHOSO_H
 #define SOSPECHOSO_H
-
-
+#include <string>
+#include <vector>
+using namespace std;
 
 class Sospechoso {
+public:
+    string nombre;
+    vector<string> atributos;
+    vector<string> revelados;
+    bool culpable;
+    Sospechoso();
+    Sospechoso(string nombre, vector<string> atributos);
 
+    void revelar(int indice);
+    void mostrar();
 };
-
-
-
 #endif //SOSPECHOSO_H
